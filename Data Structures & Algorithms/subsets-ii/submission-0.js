11177c1d8df6fae924e2884a350ce64a24d98a15
Chander Vyas <chanderp031@gmail.com>
2026-05-16 12:37:00 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsetsWithDup(nums) {
+        let res = [];
+        nums = nums.sort((a,b) => a-b);
+
+        const backtracking = (start, current) => {
+            
+            //  every subset is valid
+            res.push([...current]);
+
+            for (let i = start; i < nums.length; i++) {
+                // skip the duplicates
+                if (i > start && nums[i] === nums[i-1]) continue;
+
+                current.push(nums[i]);
+                backtracking(i+1, current);
+                current.pop();
+            }   
+        }
+
+        backtracking(0, []);
+        return res;
+
+    }
+}
